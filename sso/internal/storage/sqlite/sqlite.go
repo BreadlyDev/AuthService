1f6332fcca6c7e0149b6a0c4b13f401c4eb30d9b
breadlydev <breadydev@gmail.com>
2024-11-18 23:04:44 +0600

    fixed important query in storage and deleted non-used protofile

--- sso/internal/storage/sqlite/sqlite.go
+++ sso/internal/storage/sqlite/sqlite.go
@@ -104,7 +104,7 @@ func (s *Storage) IsAdmin(ctx context.Context, userID int64) (bool, error) {
 func (s *Storage) App(ctx context.Context, appID int) (models.App, error) {
 	const op = "storage.sqlite.App"
 
-	stmt, err := s.db.Prepare("SELECT id, name, secret FROM")
+	stmt, err := s.db.Prepare("SELECT id, name, secret FROM apps WHERE id = ?")
 	if err != nil {
 		return models.App{}, fmt.Errorf("%s: %w", op, err)
 	}
